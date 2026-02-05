a279b61d61d8263963bbf8e6aea4a0d1c9403ed6
yunasuikai <yunasui63@gmail.com>
2026-02-05 10:59:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -9,7 +9,7 @@ import { connect } from 'cloudflare:sockets';
 let subPath = 'link';     // 节点订阅路径,不修改将使用uuid作为订阅路径
 let password = '123456';  // 主页密码,建议修改或添加 PASSWORD环境变量
 let proxyIP = 'proxy.xxxxxxxx.tk:50001';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
-let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID,建议修改或添加环境便量
+let yourUUID = '07037cb9-273b-453d-bc71-b628a356d134'; // UUID,建议修改或添加环境便量
 let disabletro = false;  // 是否关闭trojan, 设置为true时关闭，false开启 
 
 // CDN 
